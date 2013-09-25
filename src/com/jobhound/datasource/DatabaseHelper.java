0635d5a2fc221a3e9dc7e8486bda02b7bd088b8c
GoldenCrow <peter_14_@hotmail.co.uk>
2013-09-25 14:14:15 +0100

    Attempting to fix DB helper

--- src/com/jobhound/datasource/DatabaseHelper.java
+++ src/com/jobhound/datasource/DatabaseHelper.java
@@ -18,7 +18,7 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private static final String DATABASE_NAME = "jobhound.db";
      
     // any time you make changes to your database objects, you may have to increase the database version
-    private static final int DATABASE_VERSION = 0;
+    private static final int DATABASE_VERSION = 1;
 	
     
     // the DAO object we use to access the SimpleData table
